ab796e5d1f3fc7191967a20c2514fdc4321c5d99
Eduarda Verçosa <46230649+eduardavercosa@users.noreply.github.com>
2021-06-08 11:31:42 -0300

    Update forms.py

--- backend/battling/forms.py
+++ backend/battling/forms.py
@@ -59,7 +59,6 @@ class CreateTeamForm(forms.ModelForm):
 
     def save(self, commit=True):
         data = self.clean()
-        pokemon = self.instance.pokemons
 
         PokemonTeam.objects.create(team=self.instance, pokemon=data["pokemon_1"], order=1)
         PokemonTeam.objects.create(team=self.instance, pokemon=data["pokemon_2"], order=2)
