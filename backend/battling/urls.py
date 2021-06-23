9f62d3ceb8cf44130afb7c28ac62f493e3f5f116
Eduarda <eduardavercosa@vinta.com.br>
2021-06-22 21:41:16 -0300

    feat: user can signin

--- backend/battling/urls.py
+++ backend/battling/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from battling.views import BattleList, CreateBattle, CreateTeam, DeleteBattle, DetailBattle, Home
+from users.views import Signup
 
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
+    path("signup/", Signup.as_view(), name="signup"),
     path("battle/new/", CreateBattle.as_view(), name="create_battle"),
     path("battle/list/", BattleList.as_view(), name="battle_list"),
     path("team/<int:pk>/edit/", CreateTeam.as_view(), name="create_team"),
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,19 @@
+from django.contrib.auth import login
 from django.shortcuts import render  # noqa
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+
+from .forms import SignUpForm
 
 
-# Create your views here.
+class Signup(FormView):
+    template_name = "auth/signup.html"
+    form_class = SignUpForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(Signup, self).form_valid(form)
