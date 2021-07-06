3055116b3a62cb25c5864e1f69cb20e7b05f006e
Eduarda <eduardavercosa@vinta.com.br>
2021-07-06 09:23:20 -0300

    fix: remove debug_toolbar

--- backend/duda/urls.py
+++ backend/duda/urls.py
@@ -1,9 +1,7 @@
-from django.conf import settings
 from django.conf.urls import include, url  # noqa
 from django.contrib import admin
 from django.urls import path
 
-import debug_toolbar
 import django_js_reverse.views
 
 
@@ -12,8 +10,3 @@ urlpatterns = [
     path("jsreverse/", django_js_reverse.views.urls_js, name="js_reverse"),
     path("", include("battling.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += [
-        path("__debug__/", include(debug_toolbar.urls)),
-    ]
