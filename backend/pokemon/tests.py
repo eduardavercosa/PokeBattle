02cc81a9b9421b75f008abe7b6104e027c3b59ce
Eduarda <eduardavercosa@vinta.com.br>
2021-07-20 15:28:59 -0300

    feat: unit test

--- backend/pokemon/tests.py
+++ backend/pokemon/tests.py
@@ -1,4 +1,23 @@
-# from django.test import TestCase
+from django.test import TestCase
+
+from pokemon.helpers import get_pokemon_from_api, is_team_valid
 
 
-# Create your tests here.
+class PokemonSumTest(TestCase):
+    def test_sum_success(self):
+        pokemon = [
+            get_pokemon_from_api("pidgeot"),
+            get_pokemon_from_api("pidgey"),
+            get_pokemon_from_api("pidgeotto"),
+        ]
+        sum_pokemon = is_team_valid(pokemon)
+        self.assertTrue(sum_pokemon)
+
+    def test_sum_fail(self):
+        pokemon = [
+            get_pokemon_from_api("pidgeot"),
+            get_pokemon_from_api("beedrill"),
+            get_pokemon_from_api("pidgeotto"),
+        ]
+        sum_pokemon = is_team_valid(pokemon)
+        self.assertFalse(sum_pokemon)
