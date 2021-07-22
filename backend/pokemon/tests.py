ae1774dfecf13afec0b26bb05decc5b00d242f50
Eduarda <eduardavercosa@vinta.com.br>
2021-07-22 13:45:46 -0300

    fix: unit test

--- backend/pokemon/tests.py
+++ backend/pokemon/tests.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 from model_bakery import baker
 
 from pokemon.helpers import is_team_valid
-from pokemon.models import Pokemon
 
 
 class PokemonSumTest(TestCase):
@@ -44,28 +43,28 @@ class PokemonSumTest(TestCase):
 
     def test_team_sum(self):
         pokemon_1 = {
-            "poke_id": Pokemon.objects.get(poke_id=15).poke_id,
-            "defense": Pokemon.objects.get(poke_id=15).defense,
-            "attack": Pokemon.objects.get(poke_id=15).attack,
-            "hp": Pokemon.objects.get(poke_id=15).hp,
+            "poke_id": self.pokemon_1.poke_id,
+            "defense": self.pokemon_1.defense,
+            "attack": self.pokemon_1.attack,
+            "hp": self.pokemon_1.hp,
         }
         pokemon_2 = {
-            "poke_id": Pokemon.objects.get(poke_id=16).poke_id,
-            "defense": Pokemon.objects.get(poke_id=16).defense,
-            "attack": Pokemon.objects.get(poke_id=16).attack,
-            "hp": Pokemon.objects.get(poke_id=16).hp,
+            "poke_id": self.pokemon_2.poke_id,
+            "defense": self.pokemon_2.defense,
+            "attack": self.pokemon_2.attack,
+            "hp": self.pokemon_2.hp,
         }
         pokemon_3 = {
-            "poke_id": Pokemon.objects.get(poke_id=17).poke_id,
-            "defense": Pokemon.objects.get(poke_id=17).defense,
-            "attack": Pokemon.objects.get(poke_id=17).attack,
-            "hp": Pokemon.objects.get(poke_id=17).hp,
+            "poke_id": self.pokemon_3.poke_id,
+            "defense": self.pokemon_3.defense,
+            "attack": self.pokemon_3.attack,
+            "hp": self.pokemon_3.hp,
         }
         pokemon_4 = {
-            "poke_id": Pokemon.objects.get(poke_id=18).poke_id,
-            "defense": Pokemon.objects.get(poke_id=18).defense,
-            "attack": Pokemon.objects.get(poke_id=18).attack,
-            "hp": Pokemon.objects.get(poke_id=18).hp,
+            "poke_id": self.pokemon_4.poke_id,
+            "defense": self.pokemon_4.defense,
+            "attack": self.pokemon_4.attack,
+            "hp": self.pokemon_4.hp,
         }
 
         pokemon_team_1 = [
