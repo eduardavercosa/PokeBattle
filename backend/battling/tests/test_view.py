2b89bf0eff186b76a51bcb297354cb8e16d0f752
Eduarda <eduardavercosa@vinta.com.br>
2021-07-22 13:21:36 -0300

    fix: create battle test

--- backend/battling/tests/test_view.py
+++ backend/battling/tests/test_view.py
@@ -7,19 +7,28 @@ from pokemon.models import Pokemon
 from users.models import User
 
 
-class ListBattlesTest(TestCase):
+class CreateBattleTest(TestCase):
     def setUp(self):
+        super().setUp()
         self.client = Client()
-        self.user = User.objects.create(email="eduardavercosa@vinta.com.br", password="admin")
-        self.user.set_password("admin")
-        self.user.save()
+        self.creator = baker.make("users.User")
+        self.creator.set_password("admin")
+        self.creator.save()
+        self.opponent = baker.make("users.User")
 
     def test_create_battle(self):
-        self.client.login(username=self.user.email, password="admin")
-        battles = baker.make("battling.Battle", creator=self.user, _quantity=2)
-        response = self.client.get("/battle/list/")
+        self.client.login(username=self.creator.email, password="admin")
+        battle = baker.make(
+            "battling.Battle", creator=self.creator, opponent=self.opponent, _quantity=2
+        )
+        battle_data = {
+            "creator": self.creator,
+            "opponent": self.opponent,
+        }
+
+        response = self.client.get("/battle/list/", battle_data)
         response_qs = response.context_data.get("battle_list")
-        self.assertCountEqual(battles, response_qs)
+        self.assertCountEqual(battle, response_qs)
 
 
 class CreatePokemonTest(TestCase):
