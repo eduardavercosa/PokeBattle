7a568d60292c155c412ea67d6a042ac939476514
Eduarda <eduardavercosa@vinta.com.br>
2021-07-26 15:25:15 -0300

    fix: delete not-needed tests

--- backend/battling/tests/test_view.py
+++ backend/battling/tests/test_view.py
@@ -27,11 +27,6 @@ class CreateBattleViewTest(TestCase):
 
         self.assertFalse(battle)
 
-    def test_create_zero_battles(self):
-        self.client.login(username=self.creator.email, password="admin")
-        battle = Battle.objects.filter(creator=self.creator, opponent=self.opponent)
-        self.assertFalse(battle)
-
     def test_create_one_battle(self):
         battle_data = {
             "creator": self.creator.id,
@@ -42,40 +37,6 @@ class CreateBattleViewTest(TestCase):
         self.client.post(reverse("create_battle"), battle_data)
         battle = Battle.objects.filter(creator=self.creator, opponent=self.opponent)
 
-        self.assertTrue(battle)
-
-        for _, single_battle in enumerate(battle):
-            self.assertCountEqual(single_battle.creator.email, self.creator.email)
-            self.assertCountEqual(single_battle.opponent.email, self.opponent.email)
-
-    def test_create_a_few_battles(self):
-        battle_data = {
-            "creator": self.creator.id,
-            "opponent": self.opponent.email,
-        }
-
-        self.client.login(username=self.creator.email, password="admin")
-        for _ in range(5):
-            self.client.post(reverse("create_battle"), battle_data)
-
-        battle = Battle.objects.filter(creator=self.creator, opponent=self.opponent)
-        self.assertTrue(battle)
-
-        for _, single_battle in enumerate(battle):
-            self.assertCountEqual(single_battle.creator.email, self.creator.email)
-            self.assertCountEqual(single_battle.opponent.email, self.opponent.email)
-
-    def test_create_a_lot_of_battles(self):
-        battle_data = {
-            "creator": self.creator.id,
-            "opponent": self.opponent.email,
-        }
-
-        self.client.login(username=self.creator.email, password="admin")
-        for _ in range(50):
-            self.client.post(reverse("create_battle"), battle_data)
-
-        battle = Battle.objects.filter(creator=self.creator, opponent=self.opponent)
         self.assertTrue(battle)
 
         for _, single_battle in enumerate(battle):
