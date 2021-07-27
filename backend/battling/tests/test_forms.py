db3798e5c201a58f2033e9f753a69012e755c8bb
Eduarda <eduardavercosa@vinta.com.br>
2021-07-27 16:41:49 -0300

    feat: create team form test

---
+++ backend/battling/tests/test_forms.py
@@ -0,0 +1,74 @@
+from battling.forms import CreateTeamForm
+from common.utils.tests import TestCaseUtils
+
+
+class CreateTeamFormTest(TestCaseUtils):
+    def test_create_team_form_with_valid_data(self):
+        form = CreateTeamForm(
+            data={
+                "pokemon_1": "pikachu",
+                "pokemon_1_position": "1",
+                "pokemon_2": "pidgey",
+                "pokemon_2_position": "2",
+                "pokemon_3": "beedrill",
+                "pokemon_3_position": "3",
+            }
+        )
+
+        self.assertTrue(form.is_valid())
+
+    def test_create_team_form_with_repeated_position(self):
+        form = CreateTeamForm(
+            data={
+                "pokemon_1": "pikachu",
+                "pokemon_1_position": "2",
+                "pokemon_2": "pidgey",
+                "pokemon_2_position": "2",
+                "pokemon_3": "beedrill",
+                "pokemon_3_position": "3",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+
+    def test_create_team_form_with_repeated_pokemon(self):
+        form = CreateTeamForm(
+            data={
+                "pokemon_1": "pikachu",
+                "pokemon_1_position": "1",
+                "pokemon_2": "pikachu",
+                "pokemon_2_position": "2",
+                "pokemon_3": "beedrill",
+                "pokemon_3_position": "3",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+
+    def test_create_team_form_with_more_than_600_points(self):
+        form = CreateTeamForm(
+            data={
+                "pokemon_1": "pidgeotto",
+                "pokemon_1_position": "1",
+                "pokemon_2": "pidgeot",
+                "pokemon_2_position": "2",
+                "pokemon_3": "beedrill",
+                "pokemon_3_position": "3",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+
+    def test_create_team_form_with_invalid_pokemon(self):
+        form = CreateTeamForm(
+            data={
+                "pokemon_1": "invalid",
+                "pokemon_1_position": "1",
+                "pokemon_2": "pidgey",
+                "pokemon_2_position": "2",
+                "pokemon_3": "beedrill",
+                "pokemon_3_position": "3",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
