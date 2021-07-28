0df9da9f7c763c0debb37a1c353ae43ef694c9ba
Eduarda <eduardavercosa@vinta.com.br>
2021-07-28 11:25:53 -0300

    feat: missing data test

--- backend/battling/tests/test_forms.py
+++ backend/battling/tests/test_forms.py
@@ -17,6 +17,39 @@ class CreateTeamFormTest(TestCaseUtils):
 
         self.assertTrue(form.is_valid())
 
+    def test_create_team_form_without_data(self):
+        form = CreateTeamForm(data={})
+
+        self.assertFalse(form.is_valid())
+
+    def test_create_team_form_with_missing_pokemon(self):
+        form = CreateTeamForm(
+            data={
+                "pokemon_1": "",
+                "pokemon_1_position": "1",
+                "pokemon_2": "pidgey",
+                "pokemon_2_position": "2",
+                "pokemon_3": "beedrill",
+                "pokemon_3_position": "3",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+
+    def test_create_team_form_with_missing_position(self):
+        form = CreateTeamForm(
+            data={
+                "pokemon_1": "pikachu",
+                "pokemon_1_position": "",
+                "pokemon_2": "pidgey",
+                "pokemon_2_position": "2",
+                "pokemon_3": "beedrill",
+                "pokemon_3_position": "3",
+            }
+        )
+
+        self.assertFalse(form.is_valid())
+
     def test_create_team_form_with_repeated_position(self):
         form = CreateTeamForm(
             data={
