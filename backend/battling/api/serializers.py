c10615df6e14a0cc0013b0c4a99f1e6551fb052f
Eduarda <eduardavercosa@vinta.com.br>
2021-08-04 14:02:39 -0300

    feat: battle deatil endpoint

--- backend/battling/api/serializers.py
+++ backend/battling/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from battling.models import Battle
+from battling.models import Battle, PokemonTeam, Team
+from pokemon.models import Pokemon
 from users.models import User
 
 
@@ -10,7 +11,71 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ("email", "id")
 
 
+class PokemonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pokemon
+        fields = ("poke_id", "name", "img_url", "attack", "defense", "hp")
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    pokemon_1 = PokemonSerializer()
+    pokemon_2 = PokemonSerializer()
+    pokemon_3 = PokemonSerializer()
+
+    class Meta:
+        model = PokemonTeam
+        fields = ("pokemon_1", "pokemon_2", "pokemon_3")
+
+
 class BattleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Battle
         fields = ("id", "creator", "opponent", "winner")
+
+
+class BattleDetailSerializer(serializers.ModelSerializer):
+    creator = UserSerializer()
+    creator_team = serializers.SerializerMethodField()
+    opponent = UserSerializer()
+    opponent_team = serializers.SerializerMethodField()
+    winner = UserSerializer()
+
+    def get_creator_team(self, obj):
+        creator_team = Team.objects.filter(battle=obj, trainer=obj.creator).first()
+        creator_pokemon_list = PokemonTeam.objects.filter(team=creator_team).first()
+
+        if not creator_team:
+            return {}
+
+        creator_serializer = TeamSerializer(instance=creator_pokemon_list)
+
+        return creator_serializer.data
+
+    def get_opponent_team(self, obj):
+        opponent_team = Team.objects.filter(battle=obj, trainer=obj.opponent).first()
+        opponent_pokemon_list = PokemonTeam.objects.filter(team=opponent_team).first()
+
+        if not opponent_team:
+            return {}
+
+        opponent_serializer = TeamSerializer(instance=opponent_pokemon_list)
+
+        return opponent_serializer.data
+
+    def get_winner(self, obj):
+        if obj.status == "ONGOING":
+            return {}
+
+        winner = obj.winner
+
+        return winner
+
+    class Meta:
+        model = Battle
+        fields = (
+            "creator",
+            "creator_team",
+            "opponent",
+            "opponent_team",
+            "winner",
+        )
--- backend/battling/api/urls.py
+++ backend/battling/api/urls.py
@@ -5,4 +5,5 @@ from battling.api import endpoints
 
 urlpatterns = [
     path("battles/", endpoints.BattleList.as_view(), name="battle-list"),
+    path("battles/<int:pk>/", endpoints.BattleDetail.as_view(), name="battle-detail"),
 ]
