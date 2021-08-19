1d92fd81394e10aef36787075816fcbe434b2430
Eduarda <eduardavercosa@vinta.com.br>
2021-08-19 13:50:51 -0300

    feat: add detail url

--- backend/duda/urls.py
+++ backend/duda/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url  # noqa
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 import django_js_reverse.views
 
@@ -10,4 +11,9 @@ urlpatterns = [
     path("jsreverse/", django_js_reverse.views.urls_js, name="js_reverse"),
     path("", include("battling.urls")),
     path("api/", include("battling.api.urls")),
+    path(
+        "react/battle/<int:pk>/detail",
+        TemplateView.as_view(template_name="react/react_template.html"),
+        name="react_template",
+    ),
 ]
