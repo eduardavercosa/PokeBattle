568eedd94857323bd6d78aa81af1d960225875fe
Eduarda <eduardavercosa@vinta.com.br>
2021-08-19 14:17:15 -0300

    feat: show battle detail

--- frontend/js/pages/Home.js
+++ frontend/js/pages/Home.js
@@ -1,16 +1,76 @@
+import { get, map } from 'lodash';
 import React, { useState } from 'react';
 
-const Home = () => {
-  const [showBugComponent, setShowBugComponent] = useState(false);
+import { apiUrls, getFromApi } from 'utils/api';
 
+function Home() {
+  const [battle, setBattle] = useState();
+  const getTeamData = async () => {
+    const data = await getFromApi(apiUrls.battleDetail(34));
+    setBattle(data);
+    return data;
+  };
+  getTeamData();
+  if (!battle) {
+    return `loading`;
+  }
   return (
-    <>
-      <button type="button" onClick={() => setShowBugComponent(true)}>
-        Click to test if Sentry is capturing frontend errors! (Should only work in Production)
-      </button>
-      {showBugComponent && showBugComponent.field.notexist}
-    </>
+    <div>
+      <p>{battle.winner.email} won!</p>
+      <p>{battle.creator.email} team:</p>
+      <div>
+        <table>
+          <tr>
+            <th>Pokemon</th>
+            <th>name</th>
+            <th>attack</th>
+            <th>defense</th>
+            <th>hp</th>
+          </tr>
+
+          {map(battle.teams[0].pokemons, (pokemon) => {
+            return (
+              <tr>
+                <th>
+                  <img alt="pokemon img" height="90px" src={get(pokemon, 'img_url')} />
+                </th>
+                <th> {pokemon.name}</th>
+                <th> {pokemon.attack}</th>
+                <th> {pokemon.defense}</th>
+                <th> {pokemon.hp}</th>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+      <p>{battle.opponent.email} team:</p>
+      <div>
+        <table>
+          <tr>
+            <th>Pokemon</th>
+            <th>name</th>
+            <th>attack</th>
+            <th>defense</th>
+            <th>hp</th>
+          </tr>
+
+          {map(battle.teams[1].pokemons, (pokemon) => {
+            return (
+              <tr>
+                <th>
+                  <img alt="pokemon img" height="90px" src={get(pokemon, 'img_url')} />
+                </th>
+                <th> {pokemon.name}</th>
+                <th> {pokemon.attack}</th>
+                <th> {pokemon.defense}</th>
+                <th> {pokemon.hp}</th>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    </div>
   );
-};
+}
 
 export default Home;
