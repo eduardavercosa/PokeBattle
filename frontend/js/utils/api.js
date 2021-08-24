6640348b68b9d5d74dd6780c0b4d1e3f478f23d5
Eduarda <eduardavercosa@vinta.com.br>
2021-08-24 16:57:21 -0300

    fix: remove extra /

--- frontend/js/utils/api.js
+++ frontend/js/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseUrl = window.location.host;
 
 const getFromApi = (apiUrls) => {
-  const url = `http://${baseUrl}/${apiUrls}`;
+  const url = `http://${baseUrl}${apiUrls}`;
   const response = axios.get(url).then((res) => {
     return res.data;
   });
