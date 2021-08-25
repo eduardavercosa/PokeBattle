45513086ca9602b6a31ef6c8b497574a08995700
Eduarda <eduardavercosa@vinta.com.br>
2021-08-25 16:34:17 -0300

    fix: change url name

--- backend/battling/api/urls.py
+++ backend/battling/api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("battles/list/", endpoints.BattleList.as_view(), name="battle-list"),
     path("battles/<int:pk>/", endpoints.BattleDetail.as_view(), name="battle-detail"),
     path("battles/create/", endpoints.CreateBattle.as_view(), name="create-battle"),
-    path("team/<int:pk>/edit/", endpoints.CreateTeam.as_view(), name="create-team"),
-    path("user/", endpoints.CurrentUserEndpoint.as_view(), name="logged-user"),
+    path("team/<int:pk>/edit/", endpoints.CreateTeam.as_view(), name="team_create"),
+    path("user/", endpoints.CurrentUserEndpoint.as_view(), name="current-user"),
 ]
