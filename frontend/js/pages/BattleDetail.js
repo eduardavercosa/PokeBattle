0defa939af6cd7ffeb7319f823e2091ceb28d081
Eduarda <eduardavercosa@vinta.com.br>
2021-08-30 18:19:29 -0300

    feat: page style

--- frontend/js/pages/BattleDetail.js
+++ frontend/js/pages/BattleDetail.js
@@ -1,26 +1,10 @@
 import { get, map } from 'lodash';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
 
 import { getFromApi } from 'utils/api';
 
 import Urls from '../utils/urls';
-
-const Title = styled.h1`
-  font-size: 3em;
-  color: white;
-`;
-
-const Text = styled.h1`
-  font-size: 1.5em;
-  color: white;
-`;
-
-const Wrapper = styled.section`
-  padding: 4em;
-  background: linear-gradient(to right, rgb(197, 230, 236), rgb(239, 187, 230));
-`;
 
 function BattleDetail() {
   const { id } = useParams();
@@ -37,89 +21,109 @@ function BattleDetail() {
   }, []);
   if (!battle) {
     return (
-      <Wrapper>
-        <Title>The battle you are looking for does not exist.</Title>
-      </Wrapper>
+      <div className="body">
+        <div className="teste">
+          <h1>The battle you are looking for does not exist.</h1>
+        </div>
+      </div>
     );
   }
   if (!battle.winner || !battle.teams) {
     return (
-      <Wrapper>
-        <Title>Battle result!</Title>
+      <div className="container2">
+        <h1>Battle result!</h1>
         <div>
-          <Text>The battle is not over yet!</Text>
+          <h1>The battle is not over yet!</h1>
           <p>{battle.creator.email} team:</p>
           <div>
             <a href={Urls['battle-list']()}>Back</a>
           </div>
         </div>
-      </Wrapper>
+      </div>
     );
   }
   return (
-    <Wrapper>
-      <Title>Battle result!</Title>
-      <div>
-        <Text>The winner is {battle.winner.email}!</Text>
-        <p>{battle.creator.email} team:</p>
-        <div>
-          <table>
-            <tbody>
-              <tr>
-                <th>Pokemon</th>
-                <th>name</th>
-                <th>attack</th>
-                <th>defense</th>
-                <th>hp</th>
-              </tr>
+    <div className="body">
+      <div className="container1" id="home">
+        <h1>Welcome to Poke Battle!</h1>
+        <h2>Choose your pokemons and fight!</h2>
+        <div className="container2">
+          <th>
+            <img
+              alt="pokemon_img"
+              height="200px"
+              src="https://static.tvtropes.org/pmwiki/pub/images/pokemon_350_210.png"
+              width="400px"
+            />
+          </th>
+          <div className="teste">
+            <h1>Result</h1>
+            <div>
+              <h3>{battle.winner.email} won!</h3>
+              <p>{battle.creator.email} team:</p>
+              <div>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Pokemon</th>
+                      <th>name</th>
+                      <th>attack</th>
+                      <th>defense</th>
+                      <th>hp</th>
+                    </tr>
 
-              {map(battle.teams[0].pokemons, (pokemon) => {
-                return (
-                  <tr key={pokemon.name}>
-                    <th>
-                      <img alt="pokemon img" height="90px" src={get(pokemon, 'img_url')} />
-                    </th>
-                    <th> {pokemon.name}</th>
-                    <th> {pokemon.attack}</th>
-                    <th> {pokemon.defense}</th>
-                    <th> {pokemon.hp}</th>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-        <p>{battle.opponent.email} team:</p>
-        <div>
-          <table>
-            <tbody>
-              <tr>
-                <th>Pokemon</th>
-                <th>name</th>
-                <th>attack</th>
-                <th>defense</th>
-                <th>hp</th>
-              </tr>
+                    {map(battle.teams[0].pokemons, (pokemon) => {
+                      return (
+                        <tr key={pokemon.name}>
+                          <th>
+                            <img alt="pokemon img" height="90px" src={get(pokemon, 'img_url')} />
+                          </th>
+                          <th> {pokemon.name}</th>
+                          <th> {pokemon.attack}</th>
+                          <th> {pokemon.defense}</th>
+                          <th> {pokemon.hp}</th>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+              <p>{battle.opponent.email} team:</p>
+              <div>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Pokemon</th>
+                      <th>name</th>
+                      <th>attack</th>
+                      <th>defense</th>
+                      <th>hp</th>
+                    </tr>
 
-              {map(battle.teams[1].pokemons, (pokemon) => {
-                return (
-                  <tr key={pokemon.name}>
-                    <th>
-                      <img alt="pokemon img" height="90px" src={get(pokemon, 'img_url')} />
-                    </th>
-                    <th> {pokemon.name}</th>
-                    <th> {pokemon.attack}</th>
-                    <th> {pokemon.defense}</th>
-                    <th> {pokemon.hp}</th>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+                    {map(battle.teams[1].pokemons, (pokemon) => {
+                      return (
+                        <tr key={pokemon.name}>
+                          <th>
+                            <img alt="pokemon img" height="90px" src={get(pokemon, 'img_url')} />
+                          </th>
+                          <th> {pokemon.name}</th>
+                          <th> {pokemon.attack}</th>
+                          <th> {pokemon.defense}</th>
+                          <th> {pokemon.hp}</th>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <a className="button_next" href={Urls['battle-list']()}>
+              Back
+            </a>
+          </div>
         </div>
       </div>
-      <a href={Urls['battle-list']()}>Back</a>
-    </Wrapper>
+    </div>
   );
 }
 
