e0df9bdfca29394dab2e85d02acdeadcc0ef7af8
Eduarda <eduardavercosa@vinta.com.br>
2021-09-06 15:05:51 -0300

    fix: getFromApi parameter

--- frontend/js/utils/api.js
+++ frontend/js/utils/api.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
-import Urls from './urls';
-
 const baseUrl = window.location.host;
 
-const urlApi = Urls['battle-detail'](':id');
-
-const getFromApi = () => {
-  const url = `http://${baseUrl}/${urlApi}`;
+const getFromApi = (apiUrls) => {
+  const url = `http://${baseUrl}${apiUrls}`;
   const response = axios.get(url).then((res) => {
     return res.data;
   });
