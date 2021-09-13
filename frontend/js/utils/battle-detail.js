cc3523e7bb92f039412dc0ef3f3fcfb5f6399769
Eduarda <eduardavercosa@vinta.com.br>
2021-09-13 20:20:53 -0300

    fix: improve code

--- frontend/js/utils/battle-detail.js
+++ frontend/js/utils/battle-detail.js
@@ -1,14 +1,10 @@
 const showTeams = (battle, user) => {
   let currentUserTeam = null;
   let opponentUserTeam = null;
-  if (battle.teams.length === 1) {
-    currentUserTeam = battle.teams[0].trainer.email === user.email ? battle.teams[0] : null;
-    opponentUserTeam = currentUserTeam === null ? battle.teams[0] : null;
-  } else if (battle.teams.length === 2) {
-    currentUserTeam =
-      battle.teams[0].trainer.email === user.email ? battle.teams[0] : battle.teams[1];
-    opponentUserTeam = currentUserTeam === battle.teams[0] ? battle.teams[1] : battle.teams[0];
-  }
+  currentUserTeam =
+    battle.teams[0].trainer.email === user.email ? battle.teams[0] : battle.teams[1];
+  opponentUserTeam = currentUserTeam === battle.teams[0] ? battle.teams[1] : battle.teams[0];
+
   return {
     currentUserTeam,
     opponentUserTeam,
