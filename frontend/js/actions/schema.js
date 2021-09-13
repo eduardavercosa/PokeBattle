34919261b924c747e0debf039ae543f2f905b2a2
Eduarda <eduardavercosa@vinta.com.br>
2021-09-13 20:48:12 -0300

    feat: normalize data

---
+++ frontend/js/actions/schema.js
@@ -0,0 +1,7 @@
+import { schema } from 'normalizr';
+
+export const userSchema = new schema.Entity('user');
+
+export const pokemonSchema = new schema.Entity('pokemon');
+
+export const battleSchema = new schema.Entity('battle');
