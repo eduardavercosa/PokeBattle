d89a01144df1849e9d1fbac8c5b449d1b410487a
Eduarda <eduardavercosa@vinta.com.br>
2021-09-20 18:20:45 -0300

    fix: no teams validation

--- frontend/js/utils/battle-detail.js
+++ frontend/js/utils/battle-detail.js
@@ -1,7 +1,7 @@
 const showTeams = (battle, user) => {
   let currentUserTeam = null;
   let opponentUserTeam = null;
-  if (battle.teams) {
+  if (battle.teams.length === 2) {
     currentUserTeam =
       battle.teams[0].trainer.email === user.email ? battle.teams[0] : battle.teams[1];
     opponentUserTeam = currentUserTeam === battle.teams[0] ? battle.teams[1] : battle.teams[0];
