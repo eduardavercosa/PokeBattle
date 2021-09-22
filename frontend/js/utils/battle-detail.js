f2a9fd36adf3080fc5d1060e85a2f374e08d6d55
Eduarda <eduardavercosa@vinta.com.br>
2021-09-22 12:20:47 -0300

    fix: empty spaces

--- frontend/js/utils/battle-detail.js
+++ frontend/js/utils/battle-detail.js
@@ -1,7 +1,7 @@
 const showTeams = (battle, user) => {
   let currentUserTeam = null;
   let opponentUserTeam = null;
-  
+
   if (battle.teams.length === 2) {
     currentUserTeam =
       battle.teams[0].trainer.email === user.email ? battle.teams[0] : battle.teams[1];
